e4d838455b4e5e8900448b9722643c3f741a2a2b
Soham Pramanik <soham.workspace1@gmail.com>
2026-08-29 23:12:50 +0530

    mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("MongoDB Connected");
+    console.log("MongoDB Connected ✅");
   } catch (error) {
     console.log(error);
     process.exit(1);
