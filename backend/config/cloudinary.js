0d641950f241b4b5bb074d912137040454a70540
Soham Pramanik <soham.workspace1@gmail.com>
2026-09-22 00:24:48 +0530

    config

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,18 +1,3 @@
-// const cloudinary = require("cloudinary").v2;
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// console.log("Cloudinary configuration loaded:");
-// console.log("Cloud name:", process.env.CLOUDINARY_CLOUD_NAME ? "YES" : "NO");
-// console.log("API key:", process.env.CLOUDINARY_API_KEY ? "YES" : "NO");
-// console.log("API secret:", process.env.CLOUDINARY_API_SECRET ? "YES" : "NO");
-
-// module.exports = cloudinary;
-
 const cloudinary = require("cloudinary").v2;
 
 console.log("========== CLOUDINARY CONFIG ==========");
